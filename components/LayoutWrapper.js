7a8ba18d675d2e3dc68e20c15356d8f1b0120077
Surender Nukala <nukala.surender@gmail.com>
2021-04-07 17:37:05 +0530

    initial commit

---
+++ components/LayoutWrapper.js
@@ -0,0 +1,52 @@
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.svg'
+import Link from './Link'
+import SectionContainer from './SectionContainer'
+import Footer from './Footer'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+
+const LayoutWrapper = ({ children }) => {
+  return (
+    <SectionContainer>
+      <div className="flex flex-col justify-between h-screen">
+        <header className="flex items-center justify-between py-10">
+          <div>
+            <Link href="/" aria-label="Nukala Surendhar">
+              <div className="flex items-center justify-between">
+
+                {typeof siteMetadata.headerTitle === 'string' ? (
+                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                    { ` $ cd / home/ ` } <span className="text-red-600"></span>
+                  </div>
+                ) : (
+                  siteMetadata.headerTitle
+                )}
+              </div>
+            </Link>
+          </div>
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="font-medium hover:border-2 hover:rounded-lg transition-colors duration-100 ease-in-out hover:bg-teal-400 hover:text-blue-600 text-gray-900 sm:p-4 sm:py-2 dark:text-gray-100"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <ThemeSwitch />
+            <MobileNav />
+          </div>
+        </header>
+        <main className="mb-auto">{children}</main>
+        <Footer />
+      </div>
+    </SectionContainer>
+  )
+}
+
+export default LayoutWrapper
---
+++ data/headerNavLinks.js
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/blog', title: '.blog' },
+  { href: '/tags', title: '_tags' },
+  { href: '/projects', title: 'projects()' },
+  { href: '/about', title: 'about()' },
+]
+
+export default headerNavLinks
